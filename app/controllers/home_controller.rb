ebd8efa5757e3c770560d02acc4cc55cd74f36b4
unknown <peters0470@gmail.com>
2021-06-19 20:00:38 -0500

    added navbar and started layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = ''
+    @uri =  URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
   end
 end
